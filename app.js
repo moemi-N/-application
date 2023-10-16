48779a70769fe874f8e56775f7e43c095c563c52
moe <pinktulip1027@gmail.com>
2023-10-16 23:28:43 +0900

    1連の呼吸の動作が1ッ回は表示された。

---
+++ app.js
@@ -0,0 +1,22 @@
+// containerが膨らんだり縮んだりを取得.一致する最初の要素を返す。
+const container = document.querySelector(".container")
+const text = document.getElementById("text");
+
+let totalTime = 7500;
+let breatheTime = (totalTime/5)*2;
+let holdtime = totalTime/5;
+
+function meditateAnimation(){
+        text.innerHTML ="inhale";
+        container.className = "container big";
+    // breatheTime分の時間が経過したらsetTimeoutを実行する。
+    setTimeout(() => {
+        text.innerHTML = "Hold";
+
+        setTimeout(() =>{
+            text.innerHTML = "exhale";
+            container.className = "container small";
+        }, holdtime);
+    }, breatheTime);
+}
+meditateAnimation();
